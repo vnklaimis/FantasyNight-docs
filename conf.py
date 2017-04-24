ee1278268281253a4de65f2655522cdbfe0b46a7
Chaos <bmazoka@gmail.com>
2017-04-23 18:28:39 -0600

    Testing crate theme

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 from recommonmark.parser import CommonMarkParser
-
+from crate.theme.rtd.conf import *
 
 # -- General configuration ------------------------------------------------
 
@@ -92,6 +92,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_theme = 'crate-docs-theme'
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
